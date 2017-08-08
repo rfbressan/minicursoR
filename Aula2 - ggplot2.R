745b86674b8143a59e2aff779fe226c26d71051e
rfbressan <rfbressan@gmail.com>
2017-08-08 07:04:30 -0300

    Dia 1

--- Aula2 - ggplot2.R
+++ Aula2 - ggplot2.R
@@ -2,4 +2,12 @@
 ## Aula 2
 ## ggplot2
 ###########################################################################################################
-library(ggplot2)
+library(tidyverse)
+
+dados <- tibble(x = 1:100, y = x*0.5+rnorm(100, 25, 5))
+ggplot(data = dados, mapping = aes(x = x, y = y)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+  
+amostras100 <- as.tibble(read.csv2(file = "100_amostras.csv", header = TRUE, dec = "."))
+sexo <- as.tibble(read.csv2("sexo.csv"))
