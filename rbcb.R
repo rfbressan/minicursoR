d4e9e9c233e6a269211d35e9ba538278cc7daa3b
rfbressan <rfbressan@gmail.com>
2017-08-12 18:31:12 -0300

    Cores customizadas no ggplot+geom_line

--- rbcb.R
+++ rbcb.R
@@ -4,19 +4,13 @@ library(dplyr)
 library(ggplot2)
 inicio <- as.Date("2016-01-01")
 fim <- as.Date("2016-12-01")
-<<<<<<< HEAD
-# ipca16 <- get_series(code = 443,
-#                      start_date = inicio,
-#                      end_date = fim,
-#                      name = "ipca", 
-#                      as = "tibble")
-=======
+
 ipca16 <- get_series(code = 443,
                      start_date = inicio,
                      end_date = fim,
                      name = "ipca",
                      as = "tibble")
->>>>>>> 4dc61fb0782cba6c704949f23bcbd1a804849b43
+
 nr <- 11
 nc <- 7
 datas <- seq(as.Date("2017-01-01"), length.out = nr, by = "months")
@@ -25,7 +19,6 @@ df_datas <- cbind(datas, df)
 colnames(df_datas) <- c("Data", paste("col", 1:nc, sep = "_"))
 df_datas
 
-<<<<<<< HEAD
 ipca_por_grupos <- readRDS("ipca.rds")
 
 ipca_plot <- ipca_por_grupos %>% 
@@ -33,12 +26,7 @@ ipca_plot <- ipca_por_grupos %>%
   filter(grupo %in% c("ipca", "alimentacao", "habitacao", "transportes"))
 
 plot <- ggplot(ipca_plot, aes(x = data, y = variacao))+
-  geom_line(aes(color = grupo))
+  geom_line(aes(color = grupo))+
+  scale_colour_manual(values = c('green','red','yellow', 'black'))
 plot
-=======
-## Salvar em um arquivo RDS e ler depois ##################################################################
-# saveRDS(object, file = "", ascii = FALSE, version = NULL,
-#         compress = TRUE, refhook = NULL)
-# 
-# readRDS(file, refhook = NULL)
->>>>>>> 4dc61fb0782cba6c704949f23bcbd1a804849b43
+
